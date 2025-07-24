407f4865bd69024c01e6ec7fd4d509a62d8284a2
unknown <rxrc1819@gmail.com>
2025-07-23 23:08:46 -0500

    Route Admin\Doctor

--- resources/views/admin/patients/index.blade.php
+++ resources/views/admin/patients/index.blade.php
@@ -9,13 +9,7 @@
 ]
 ]">
 
-    <x-slot name="action">
-        <x-wire-button blue href="{{route('admin.patients.create')}}">
-            <i class="fas fa-plus"></i>
-            Nuevo
-        </x-wire-button>
-
-    </x-slot>
+    
     @livewire('admin.datatables.patient-table')
 
 </x-admin-layout>
--- routes/admin.php
+++ routes/admin.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\DoctorController;
 use App\Http\Controllers\Admin\PatientController;
 use App\Http\Controllers\Admin\RoleController;
 use App\Http\Controllers\Admin\UserController;
@@ -13,3 +14,4 @@ Route::get('/', function () {
 Route::resource('roles', RoleController::class);
 Route::resource('users', UserController::class);
 Route::resource('patients', PatientController::class);
+Route::resource('doctors', DoctorController::class);
