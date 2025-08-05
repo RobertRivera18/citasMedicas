2b2e59948dbff1083d87694b1ddb137a63a4cfec
unknown <rxrc1819@gmail.com>
2025-08-05 00:40:12 -0500

    Manager Schedule Final

--- resources/views/livewire/admin/schedule-manager.blade.php
+++ resources/views/livewire/admin/schedule-manager.blade.php
@@ -20,65 +20,67 @@
                         </th>
 
                         @foreach ($days as $day)
-                        <th class="px-6 py-3 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">
-                            {{$day}}
-                        </th>
+                            <th class="px-6 py-3 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">
+                                {{ $day }}
+                            </th>
                         @endforeach
 
                     </tr>
                 </thead>
                 <tbody class="divide-y divide-gray-200">
                     @foreach ($this->hourBlocks as $hourBlock)
+                        @php
+                            $hour = $hourBlock->format('H:i:s');
+                        @endphp
 
-                    @php
-                    $hour = $hourBlock->format('H:i:s');
-                    @endphp
-
-                    <tr>
-                        <td class="px-6 py-4 whitespace-nowrap">
-                            <label>
-                                <input type="checkbox"
-                                    class="h-4 w-4 text-blue-600 border-gray-300 rounded focus:ring-blue-500">
-                                <span class="font-bold ml-2">
-                                    {{ $hour }}
-                                </span>
-                            </label>
-                        </td>
-
-                        @foreach ($days as $indexDay => $day)
-                        <td class="px-6 py-4 whitespace-nowrap">
-                            <div class="flex flex-col space-y-2">
+                        <tr>
+                            <td class="px-6 py-4 whitespace-nowrap">
                                 <label>
                                     <input type="checkbox"
-                                        x-on:click="toggleHourBlock('{{$indexDay}}','{{$hour}}',$el.chechked)"
+                                   x-on:click="toggleFullHourBlock('{{ $hour }}', $el.checked)"
+                                   :checked="isFullHourBlockChecked('{{$hour}}')"
                                         class="h-4 w-4 text-blue-600 border-gray-300 rounded focus:ring-blue-500">
-                                    <span class="ml-2 text-sm font-medium text-gray-700">
-                                        Todos
+                                    <span class="font-bold ml-2">
+                                        {{ $hour }}
                                     </span>
                                 </label>
+                            </td>
 
-                                @for($i = 0; $i < $intervals; $i++) @php $startTime=$hourBlock->copy()->addMinutes($i *
-                                    $apointment_duration);
-                                    $endTime = $startTime->copy()->addMinutes($apointment_duration);
-                                    @endphp
+                            @foreach ($days as $indexDay => $day)
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    <div class="flex flex-col space-y-2">
+                                        <label>
+                                            <input type="checkbox"
+                                                x-on:click="toggleHourBlock('{{ $indexDay }}','{{ $hour }}',$el.checked)"
+                                                :checked="isHourBlockChecked('{{ $indexDay }}', '{{ $hour }}')"
+                                                class="h-4 w-4 text-blue-600 border-gray-300 rounded focus:ring-blue-500">
+                                            <span class="ml-2 text-sm font-medium text-gray-700">
+                                                Todos
+                                            </span>
+                                        </label>
 
-                                    <label>
-                                        <input type="checkbox" {{--
+                                        @for ($i = 0; $i < $intervals; $i++)
+                                            @php
+                                                $startTime = $hourBlock->copy()->addMinutes($i * $apointment_duration);
+                                                $endTime = $startTime->copy()->addMinutes($apointment_duration);
+                                            @endphp
+
+                                            <label>
+                                                <input type="checkbox" {{--
                                             x-model="schedule['{{ $indexDay }}'][{{ $startTime->format('H:i:s') }}]"
                                             --}}
-                                            x-model="schedule['{{ $indexDay }}']['{{ $startTime->format('H:i:s') }}']"
-                                            class="h-4 w-4 text-blue-600 border-gray-300 rounded focus:ring-blue-500">
-                                        <span class="ml-2 text-sm font-medium text-gray-700">
-                                            {{$startTime->format('H:i')}} - {{ $endTime->format('H:i') }}
-                                        </span>
-                                    </label>
-                                    @endfor
-                            </div>
-                        </td>
-                        @endforeach
+                                                    x-model="schedule['{{ $indexDay }}']['{{ $startTime->format('H:i:s') }}']"
+                                                    class="h-4 w-4 text-blue-600 border-gray-300 rounded focus:ring-blue-500">
+                                                <span class="ml-2 text-sm font-medium text-gray-700">
+                                                    {{ $startTime->format('H:i') }} - {{ $endTime->format('H:i') }}
+                                                </span>
+                                            </label>
+                                        @endfor
+                                    </div>
+                                </td>
+                            @endforeach
 
-                    </tr>
-
+                        </tr>
                     @endforeach
                 </tbody>
             </table>
@@ -94,28 +96,50 @@
     </x-wire-card>
 
     @push('js')
-
-    <script>
-        function data()
-            {
+        <script>
+            function data() {
                 return {
                     schedule: @entangle('schedule'),
                     apointment_duration: @entangle('apointment_duration'),
                     intervals: @entangle('intervals'),
-                    toggleHourBlock(indexDay,hourBlock,checked){
-                        let hour =new Date(`1970-01-01T${hourBlock}`);
-                        for($i  =0 ; $i<this.intervals ; $i++){
-                            let startTime= new Date(hour.getTime()+ ($i * this.apointment_duration * 60000));
-                            let formattedStartTime=startTime.toTimeString().split('')[0];
-                             console.log(formattedStartTime);
-
+                    days: @entangle('days'),
+                    toggleHourBlock(indexDay, hourBlock, checked) {
+                        let hour = new Date(`1970-01-01T${hourBlock}`);
+                        for ($i = 0; $i < this.intervals; $i++) {
+                            let startTime = new Date(hour.getTime() + ($i * this.apointment_duration * 60000));
+                            let formattedStartTime = startTime.toTimeString().split(' ')[0];
+                            this.schedule[indexDay][formattedStartTime] = checked;
                         }
-                    }  
+                    },
+
+                    isHourBlockChecked(indexDay, hourBlock) {
+                        let hour = new Date(`1970-01-01T${hourBlock}`);
+                        for ($i = 0; $i < this.intervals; $i++) {
+                            let startTime = new Date(hour.getTime() + ($i * this.apointment_duration * 60000));
+                            let formattedStartTime = startTime.toTimeString().split(' ')[0];
+                            if (!this.schedule[indexDay][formattedStartTime]) {
+                                return false;
+                            }
+                        }
+                        return true;
+                    },
+                    toggleFullHourBlock(hourBlock, checked) {
+                       Object.keys(this.days).forEach((indexDay) => {
+                            this.toggleHourBlock(indexDay, hourBlock, checked);
+                        });
+
+                    },
+                      isFullHourBlockChecked(hourBlock)
+                    {
+                        return Object.keys(this.days).every((indexDay) => {
+                            return this.isHourBlockChecked(indexDay, hourBlock);
+                        });
+                    }
                 }
+
             }
-
-    </script>
-
+            
+        </script>
     @endpush
 
 </div>
